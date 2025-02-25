e9274880e26359f57a1c789480328b2c1bc1d9b7
amirlanqz <amirlanqz@gmail.com>
2025-02-26 02:08:35 +0500

    Refactoring - create favorites and quote modules

---
+++ src/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    text: 'Do what you can, with what you have, where you are.',
+    author: 'Theodore Roosevelt',
+    isFavorite: false,
+  },
+  {
+    text: 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+    author: 'Winston Churchill',
+    isFavorite: false,
+  },
+  {
+    text: 'In the middle of every difficulty lies opportunity.',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    text: 'It does not matter how slowly you go as long as you do not stop.',
+    author: 'Confucius',
+    isFavorite: false,
+  },
+  {
+    text: "Don't watch the clock; do what it does. Keep going.",
+    author: 'Sam Levenson',
+    isFavorite: false,
+  },
+  {
+    text: 'Strive not to be a success, but rather to be of value.',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    author: 'Theodore Roosevelt',
+    isFavorite: false,
+  },
+  {
+    text: 'The best way to predict the future is to create it.',
+    author: 'Peter Drucker',
+    isFavorite: false,
+  },
+  {
+    text: 'It always seems impossible until it’s done.',
+    author: 'Nelson Mandela',
+    isFavorite: false,
+  },
+  {
+    text: 'Act as if what you do makes a difference. It does.',
+    author: 'William James',
+    isFavorite: false,
+  },
+];
+
+export default quotes;
---
+++ src/handlers/favorites.js
@@ -0,0 +1,61 @@
+import { currentQuote } from './quote.js';
+
+function toggleFavoriteIcon(isFavorite, el) {
+  el.classList.toggle('fa', isFavorite);
+  el.classList.toggle('far', !isFavorite);
+}
+
+function showBtn(btn) {
+  btn.style.display = 'inline-block';
+}
+
+function hideBtn(btn) {
+  btn.style.display = 'none';
+}
+
+const favoritesContainer = document.getElementById('favorites-container');
+const toggleFavoriteBtn = document.getElementById('toggle-favorite-btn');
+
+function toggleFavorite() {
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteIcon(currentQuote.isFavorite, toggleFavoriteBtn);
+
+  if (currentQuote.isFavorite) {
+    showFavoriteCard(
+      currentQuote.text,
+      currentQuote.author,
+      favoritesContainer
+    );
+  } else {
+    hideFavoriteCard(currentQuote.text);
+  }
+}
+
+function hideFavoriteCard(currentQuote) {
+  const favoriteCards = document.querySelectorAll('.favorite-card');
+  favoriteCards.forEach((card) => {
+    if (card.textContent.includes(currentQuote)) {
+      card.remove();
+    }
+  });
+}
+
+function showFavoriteCard(quote, author, container) {
+  const favoriteCard = document.createElement('div');
+  favoriteCard.classList.add('favorite-card');
+  favoriteCard.innerHTML = `
+    <p>${quote}</p>
+    <p class="author">${author}</p>
+    `;
+  container.appendChild(favoriteCard);
+}
+
+function handleFavorite(isFavorite) {
+  showBtn(toggleFavoriteBtn);
+  toggleFavoriteIcon(isFavorite, toggleFavoriteBtn);
+}
+
+toggleFavoriteBtn.addEventListener('click', toggleFavorite);
+hideBtn(toggleFavoriteBtn);
+
+export { handleFavorite };
